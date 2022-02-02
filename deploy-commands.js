a143c3fbcb3d50940c1aef181683c75cd6cc7da4
ApertureCEO <42179029+ApertureCEO@users.noreply.github.com>
2022-02-02 15:11:18 -0800

    Working update 1

---
+++ deploy-commands.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { token } = require('./config.json');
+
+const clientId = '790601419333959692';
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(
+    Routes.applicationCommands(clientId),
+	{ body: commands },
+).then(() => {console.log('successfully registered all commands')})
+.catch(console.error);
